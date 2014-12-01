912187f482f277f12f86ac753f2700062f067900
Kantanart Wanichajornkul <kantanat@properyshelf.com>
2014-12-02 02:17:38 +0700

    01.12.2014
    - Set alignments balance
    - Add Doormat , Set Jquery for auto class
    - Small screen header , position icon ,info
    - Small screen Messes up Listing Details

--- js/support.js
+++ js/support.js
@@ -2,11 +2,44 @@ function move_menubar(){
     $(".menu-bottom #navigation").insertAfter( $( "#carousel-wrapper" ) );
 }
 
+/*get the existing doormat columns and translate them to the span grid system*/
+function getDoormatClass(){
+    var col_class="";
+    if($('.doormatColumn.column-0').length>0){
+        //just one column
+        col_class="col-md-12";
+    }
+    if($('.doormatColumn.column-1').length>0){
+        //two columns
+        col_class="col-md-6";
+    }
+    if($('.doormatColumn.column-2').length>0){
+        //three columns
+        col_class="col-md-4";
+    }
+    if($('.doormatColumn.column-3').length>0){
+        //four columns
+        col_class="col-md-3";
+    }
+    if($('.doormatColumn.column-4').length>0){
+        //five columns
+        col_class="";
+    }
+    if($('.doormatColumn.column-5').length>0){
+        //six columns
+        col_class="col-md-2";
+    }
+    
+    return col_class;
+}
 $(document).ready(function() {
     if($("div.carousel").length >0){
         move_menubar();
     }
-
+    if($(".doormatColumn").length >0){
+        doormat_col_class=getDoormatClass();
+        $("#footer-top-inner .doormatColumn").addClass(doormat_col_class);
+    }
 
 });
 
