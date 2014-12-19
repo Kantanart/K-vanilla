42053c8ceb2520c425d2a75aeac4fc0fc34bb3b6
Kantanart Wanichajornkul <kantanat@properyshelf.com>
2014-12-19 17:51:41 +0700

    19.12.2014
    - Header 4 style normal , light , dark , both it work
    - Fixed Pagination and style color , header style menu bottom
    - Fix bug endless (dont finish)
    - Menu bottom can swtich

--- js/palette.js
+++ js/palette.js
@@ -4,24 +4,43 @@ function InitPalette() {
      });
 }
 
-function Add_menu_bottom() {
-    $(".palette .headers ul li a.add-menu-bottom").click(function(){
-        $("body").toggleClass("menu-bottom");
+function reset_btn(){
+    $(".palette a.reset").click(function() {
+        alert("Not enabled.");
+    });
+}
+
+function move_menubottom() {
+    $(".palette .header-special .add-menu-bottom").click(function(){
+        $( "#navigation" ).insertAfter( "#carousel-wrapper" );
      });
 }
 
-function clickheaderaddclass() {
+function Add_header_normal() {
     $(".palette .headers ul li a.add-header-normal").click(function(){
-        clickHeaderClass("header-normal")
+         $("body").removeClass(" header-both header-light header-dark ");
+         $("body").addClass("header-normal");
      });
+}
+
+function Add_header_dark() {
     $(".palette .headers ul li a.add-header-dark").click(function(){
-        clickHeaderClass("header-dark")
+         $("body").removeClass("header-light header-normal header-both")
+         $("body").addClass("header-dark");
      });
-    $(".palette .headers ul li a.add-header-both").click(function(){
-        clickHeaderClass("header-both")
-     });
+}
+
+function Add_header_light() {
     $(".palette .headers ul li a.add-header-light").click(function(){
-        clickHeaderClass("header-light")
+         $("body").removeClass("header-normal header-dark header-both")
+         $("body").addClass("header-light");
+     });
+}
+
+function Add_header_both() {
+    $(".palette .headers ul li a.add-header-both").click(function(){
+         $("body").removeClass("header-light header-dark header-normal")
+         $("body").addClass("header-both");
      });
 }
 
@@ -34,6 +53,10 @@ function Checkpalette() {
 $(document).ready(function() {
     Checkpalette();
     InitPalette();
-    Add_menu_bottom();
-    clickheaderaddclass();
+    Add_header_normal();
+    Add_header_light();
+    Add_header_dark();
+    Add_header_both();
+    move_menubottom();
+    reset_btn();
 });
--- js/support.js
+++ js/support.js
@@ -1,5 +1,14 @@
+/*Improve listing bar*/
+function enhance_listingbar(){
+    //remove the ugly [ 1 ] notation and give it a class
+    $('.listingBar').html(function(i,html){
+        foo = html.replace('[','<span class="active">').replace(']','</span>');
+        return foo;
+});
+}
+
 function map_listing_data(obj){
- //use data input to give back a easy to access array for mapping
+  //use data input to give back a easy to access array for mapping
   dict=[];
   counter = $(obj).children('dd').length;
   if(counter<10){
@@ -111,7 +120,66 @@ function getDoormatClass(){
     
     return col_class;
 }
+function improve_listing_grid(){
+    //listing row
+    var width_indicator = $('#maincontent').attr('class');
+    switch(width_indicator) {
+        case 'span12':
+            //no portlet
+            text_width_indicator = 'col-md-9';
+            title_width_indicator = 'col-md-6';
+            price_width_indicator = 'col-md-3';
+            break;
+        case 'span9':
+            // one portlet
+            text_width_indicator = 'col-md-6';
+            title_width_indicator = 'col-md-4';
+            price_width_indicator = 'col-md-2';
+            break;
+        case 'span6':
+            //two portlets
+            text_width_indicator = 'col-md-3';
+            title_width_indicator = 'col-md-2';
+            price_width_indicator = 'col-md-1';
+            break;
+        default:
+            text_width_indicator = 'col-md-6';
+    }
+        $(".listing-summary").addClass('properties-rows').wrapInner('<div class="row" />');
+        $(".listing-summary .tileItem").addClass('property ' + width_indicator).wrapInner('<div class="row propertyrow" />');
+        $(".listing-summary .property .row figure").addClass('image span3').wrapInner('<div class="content" />');
+        // move image out of parent link
+        $( ".listing-summary .property .row .image .content a" ).each(function( index ) {
+            child = $(this).children('img');
+            $(this).empty().after(child);
+        });
+        //prepare listing text
+        $(".listing-summary .property .row section").addClass('body ' + text_width_indicator);
+        $( ".listing-summary .property .row .body" ).each(function( index ) {
+            dictonary = map_listing_data($(this).children('dl'));
+            dictonary.title =$(this).siblings('.tileHeadline').html();
+            dictonary.linktarget= $(this).parent().find('a:first').attr('href');
+            $(this).siblings('.tileHeadline').remove();
+            //clear existing detail structure
+            $(this).empty();
+            //set price & title
+            $(this).append('<h1 class="name-of-property">'+dictonary.title+'</h1>');
 
+            //set location
+            $(this).append('<div class="status"><a href="'+ dictonary.linktarget +'">'+dictonary.loctype+' '+dictonary.propertytype+' - '+ dictonary.listingtype +'</a></div>');
+           
+            $(this).append('<div class="location" ><div class="title"><a href="'+ dictonary.linktarget +'">'+dictonary.location+'</a></div></div>');
+            if(dictonary.type=="house"){
+                $(this).append('<div class="bedbath"><div class="bathrooms"></div><div class="value" title="Bedroom and Bathroom" >'+dictonary.bedbath+'</div></div>');    
+            }
+            else{
+                $(this).append('<div class="locationtype"><span class="key" title="Location Type" >&nbsp</span><span class="value">'+dictonary.locationtype+'</span></div>');   
+            }
+            $(this).append('<div class="area"><span class="key" title="Area" >&nbsp</span><span class="value">'+dictonary.area+'</span></div>');
+            $(this).append('<div class="price"><p class="value" >'+ dictonary.price +'</p></div>');
+            
+        });    
+}
 /*Switch Cover page - Listing search */
 function toggle_listing_type(){
     $(".listing-search-tile #formfield-form-widgets-jacuzzi span").hide('slow');
@@ -205,8 +273,21 @@ function toggle_listing_type(){
         }
     });
 }
-
 $(document).ready(function() {
+    $( document ).ajaxComplete(function() { 
+        console.log("AjaxComplate : Working");
+        $('section dl').each(function(){
+            if($(this).length>0){
+                // old style
+                improve_listing_grid();
+                enhance_listiggrid(); 
+         }
+        })
+    })
+    // only do when we have a listingbar
+    if($('.listingBar').length > 0){
+        enhance_listingbar();
+    }
     improve_site_social();
     switch_toggle();
     if($(".listing-search-tile.tile-content").length >0){
@@ -221,68 +302,11 @@ $(document).ready(function() {
         $("#footer-top-inner .doormatColumn").addClass(doormat_col_class);
     }
 
-        //listing grid
-        if($(".listing-collection-tile").length>0){
-            //prepare listing grid
-            enhance_listiggrid();      
-        }
-    //listing row
-    var width_indicator = $('#maincontent').attr('class');
-    switch(width_indicator) {
-        case 'span12':
-            //no portlet
-            text_width_indicator = 'col-md-9';
-            title_width_indicator = 'col-md-6';
-            price_width_indicator = 'col-md-3';
-            break;
-        case 'span9':
-            // one portlet
-            text_width_indicator = 'col-md-6';
-            title_width_indicator = 'col-md-4';
-            price_width_indicator = 'col-md-2';
-            break;
-        case 'span6':
-            //two portlets
-            text_width_indicator = 'col-md-3';
-            title_width_indicator = 'col-md-2';
-            price_width_indicator = 'col-md-1';
-            break;
-        default:
-            text_width_indicator = 'col-md-6';
+    //listing grid
+    if($(".listing-collection-tile").length>0){
+        //prepare listing grid
+        enhance_listiggrid();      
     }
-        $(".listing-summary").addClass('properties-rows').wrapInner('<div class="row" />');
-        $(".listing-summary .tileItem").addClass('property ' + width_indicator).wrapInner('<div class="row propertyrow" />');
-        $(".listing-summary .property .row figure").addClass('image span3').wrapInner('<div class="content" />');
-        // move image out of parent link
-        $( ".listing-summary .property .row .image .content a" ).each(function( index ) {
-            child = $(this).children('img');
-            $(this).empty().after(child);
-        });
-        //prepare listing text
-        $(".listing-summary .property .row section").addClass('body ' + text_width_indicator);
-        $( ".listing-summary .property .row .body" ).each(function( index ) {
-            dictonary = map_listing_data($(this).children('dl'));
-            dictonary.title =$(this).siblings('.tileHeadline').html();
-            dictonary.linktarget= $(this).parent().find('a:first').attr('href');
-            $(this).siblings('.tileHeadline').remove();
-            //clear existing detail structure
-            $(this).empty();
-            //set price & title
-            $(this).append('<h1 class="name-of-property">'+dictonary.title+'</h1>');
-
-            //set location
-            $(this).append('<div class="status"><a href="'+ dictonary.linktarget +'">'+dictonary.loctype+' '+dictonary.propertytype+' - '+ dictonary.listingtype +'</a></div>');
-           
-            $(this).append('<div class="location" ><div class="title"><a href="'+ dictonary.linktarget +'">'+dictonary.location+'</a></div></div>');
-            if(dictonary.type=="house"){
-                $(this).append('<div class="bedbath"><div class="bathrooms"></div><div class="value" title="Bedroom and Bathroom" >'+dictonary.bedbath+'</div></div>');    
-            }
-            else{
-                $(this).append('<div class="locationtype"><span class="key" title="Location Type" >&nbsp</span><span class="value">'+dictonary.locationtype+'</span></div>');   
-            }
-            $(this).append('<div class="area"><span class="key" title="Area" >&nbsp</span><span class="value">'+dictonary.area+'</span></div>');
-            $(this).append('<div class="price"><p class="value" >'+ dictonary.price +'</p></div>');
-            
-        });
+    improve_listing_grid();
 });
 
